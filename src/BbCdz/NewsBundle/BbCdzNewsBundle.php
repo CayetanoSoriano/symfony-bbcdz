57832e98f4f2111dcf07af4be0c48c526460216c
Cayetano <neoshadybeat@gmail.com>
2012-09-15 19:48:31 +0200

    step-2

---
+++ src/BbCdz/NewsBundle/BbCdzNewsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BbCdz\NewsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BbCdzNewsBundle extends Bundle
+{
+}
---
+++ src/bbcdz/NewsBundle/bbcdzNewsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace bbcdz\NewsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class bbcdzNewsBundle extends Bundle
+{
+}
